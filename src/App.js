2cb7951ac8afc1d71e66e42617ad8e0a4b4f742c
minki jung <qmsoqm2@gmail.com>
2022-04-30 12:05:00 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
-      <h2>xxxxxx</h2>
-      <h3>yyyyyy</h3>
-      <h3>zzzzzzzzz</h3>
+      
     </div>
   );
 }
