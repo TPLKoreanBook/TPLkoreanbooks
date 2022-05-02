0d8a9cbc06d57da0c5c50b51fb0ad82acab79bda
minki jung <qmsoqm2@gmail.com>
2022-05-02 15:03:14 -0400

    fixed error at App.js(added .js after BookList

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import BookList from './components/BookList'
-import axios from 'axios';
-import useAsync from './hooks/useAsync';
+import BookList from './components/BookList.js';
 import './App.css';
 
 
-async function getBooks() {
-  // Got it working by throwing my tpl_json.json file into the Public folder and calling it by:
-  const response = await axios.get('tpl_json.json');
-  console.log(response.data['books']);
-  return response.data;
-}
-
 function App() {
-  const [state] = useAsync(getBooks, []);
-  const { loading, data, error } = state;
 
   return (
     <div>
-      <header>
-        <h1>Hello</h1>
-      </header>
-      <main>
-        <section>
-          <BookList
-            loading={loading}
-            data={data}
-            error={error}
-          />
-        </section>
-      </main>
-      <footer>
-
-      </footer>
+      <BookList />
     </div>
   )
 };
