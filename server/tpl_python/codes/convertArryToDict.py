0ac2fe3d017880820bf22c63718942a5aabeedd7
Seungmin <shin891211@gmail.com>
2022-05-28 12:19:27 -0400

    added convert Array to Dict function for python

---
+++ server/tpl_python/codes/convertArryToDict.py
@@ -0,0 +1,18 @@
+import json
+
+with open ("server/tpl_python/data_webcrawling/googleSearchedTitles.json", 'r') as f:
+    aList = json.loads(f.read())["books"]
+
+result = []
+for items in aList:
+    data = {
+        "count": items[0],
+        "original_title": items[1],
+        "aladin_title": items[2],
+        "kyobo_title": items[3]
+    }
+    result.append(data)
+
+
+with open("googleSearchedTitles(수정).json", 'w', encoding='UTF-8') as f:
+    json.dump(result, f, indent=2, ensure_ascii=False)
