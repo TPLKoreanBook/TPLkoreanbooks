97d5df30c4be1d535a6e3624a823577a158e202d
minki jung <qmsoqm2@gmail.com>
2022-05-28 12:25:34 -0400

    final book data added

--- server/tpl_python/codes/연습.py
+++ server/tpl_python/codes/연습.py
@@ -1,3 +1,14 @@
-import constant
+# import json
 
-print(constant.TBS)
+# with open ("server/tpl_python/data_webcrawling/googleSearchedTitles.json", 'r') as f:
+#     aList = json.loads(f.read())["books"]
+# result = []
+# for titles in aList:
+#     data = {"count": titles[0],"title_origin": titles[1]}
+#     result = result.append(data)
+
+
+
+data = []
+data.append('s')
+print(data)
