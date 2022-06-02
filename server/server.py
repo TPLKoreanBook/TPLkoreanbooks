b477160b3b38050341dacc98f8291060c6ee84c6
Sejun <63370671+sejunpark2002@users.noreply.github.com>
2022-06-02 14:13:29 -0400

    test commit

--- server/server.py
+++ server/server.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 api = Api(app)
 cors = CORS(app)
 
-with open("TPLkoreanbooks/server/tpl_python/data_webcrawling/final_book_data.json", 'r') as read_file:
+with open("/Users/sejunpark/TPLkoreanbooks/server/tpl_python/data_webcrawling/final_book_data.json", 'r') as read_file:
     books = json.load(read_file)
 books = books["books"]
+
 
 def sort_by_category(category):
     if category == 'all':
@@ -21,11 +22,13 @@ def sort_by_category(category):
             result_by_category.append(book)
     return(result_by_category)
 
+
 class Category(Resource):
-    def get(self,category ):
+    def get(self, category):
         return(sort_by_category(category))
+
 
 api.add_resource(Category, "/category/<string:category>")
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
