e43d212757bb3978c4abe2e3d05576538288564c
Seungmin <shin891211@gmail.com>
2022-06-13 23:08:45 -0400

    added contents and images

--- client/src/Pages/Main.js
+++ client/src/Pages/Main.js
@@ -2,7 +2,7 @@ import styles from './Main.module.css';
 
 const Main = (props) => {
     return (
-        <main className={`wrapper ${styles['main-container']}`}>
+        <main className={styles['main-container']}>
             {props.children}
         </main>
     )
