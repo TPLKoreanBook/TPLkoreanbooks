9d4fe849b11600dc1fb1a5fffb75765aa82aff30
woong0419 <woong0419@gmail.com>
2022-08-11 16:49:52 -0400

    FE: Search bar redesign

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -2,53 +2,70 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import Categories from './Categories';
 import styles from './SearchForm.module.css';
+import { HiSearch } from 'react-icons/hi';
 
 const SearchForm = ({ categoryName, onSearch, onCategory }) => {
-    const [toggleCategory, setToggleCategory] = useState(true);
+  const [toggleCategory, setToggleCategory] = useState(true);
 
-    const categoryHash = {
-        "all": 'all',
-        'children': "어린이",
-        "youngadult": '청소년',
-        "parenting": '좋은부모',
-        "novelpoetryplay": '소설/시/희곡',
-        "essay": '에세이',
-        "humanities": '인문학',
-        "sociology": '사회과학',
-        "history": '역사',
-        "science": '과학',
-        "artculture": '예술/대중문화',
-        "religion": '종교/역학',
-        "economy": '경제경영',
-        "selfhelp": '자기계발',
-        "foreignlanguage": '외국어',
-        "homecookbeauty": '가정/요리/뷰티',
-        "healthleisure": '건강/취미/레저',
-        "etc": '기타'
-    };
-    // const newStr = categoryName.replaceAll('+', '/');
+  const categoryHash = {
+    all: 'all',
+    children: '어린이',
+    youngadult: '청소년',
+    parenting: '좋은부모',
+    novelpoetryplay: '소설/시/희곡',
+    essay: '에세이',
+    humanities: '인문학',
+    sociology: '사회과학',
+    history: '역사',
+    science: '과학',
+    artculture: '예술/대중문화',
+    religion: '종교/역학',
+    economy: '경제경영',
+    selfhelp: '자기계발',
+    foreignlanguage: '외국어',
+    homecookbeauty: '가정/요리/뷰티',
+    healthleisure: '건강/취미/레저',
+    etc: '기타',
+  };
+  // const newStr = categoryName.replaceAll('+', '/');
 
-    useEffect(() => {
-        setToggleCategory(false);
-    }, [categoryName])
-    const categoryHandler = () => {
-        setToggleCategory((prev) => !prev);
-    }
+  useEffect(() => {
+    setToggleCategory(false);
+  }, [categoryName]);
+  const categoryHandler = () => {
+    setToggleCategory((prev) => !prev);
+  };
 
-    const searchHandler = (e) => {
-        onSearch(e.target.value)
-    }
-    return (
-        <div className={styles['search-container']}>
-            <div className={styles['input-container']}>
-                <button className={styles['category-btn']} onClick={categoryHandler}>{categoryHash[categoryName]}</button>
-                <input className={styles['searchbar']} type="text" placeholder='Search...' onChange={searchHandler} />
-            </div>
-            <div className={styles['dropdown-menu']}>
-                {toggleCategory && <Categories onCategory={onCategory} />}
-            </div>
-        </div>
-    )
+  const searchHandler = (e) => {
+    onSearch(e.target.value);
+  };
+  return (
+    <div className={styles['search-container']}>
+      <div className={styles['input-container']}>
+        <button className={styles['category-btn']} onClick={categoryHandler}>
+          <p>{categoryHash[categoryName]}</p>
+        </button>
+        <div className={styles['border']}></div>
+        <input
+          className={styles['searchbar']}
+          type='text'
+          onChange={searchHandler}
+        />
+        <HiSearch
+          size='25'
+          style={{
+            position: 'absolute',
+            top: '50%',
+            right: '0',
+            transform: 'translate(-9px, -50%)',
+          }}
+        />
+      </div>
+      <div className={styles['dropdown-menu']}>
+        {toggleCategory && <Categories onCategory={onCategory} />}
+      </div>
+    </div>
+  );
 };
 
 export default SearchForm;
