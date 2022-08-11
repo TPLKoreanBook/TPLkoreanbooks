b27d58f26d7194e13fe85519a58887bab309d124
woong0419 <woong0419@gmail.com>
2022-08-11 15:25:50 -0400

    Booklist more result btn update

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -4,72 +4,95 @@ import styles from './BookList.module.css';
 import axios from 'axios';
 import useAsync from '../hooks/useAsync';
 import Book from './Book.js';
-
+import { HiChevronDown } from 'react-icons/hi';
 
 async function getBooks(category) {
-    const response = await axios.get(`https://tpl-server-heroku.herokuapp.com/${category}`);
-    return response.data;
+  const response = await axios.get(
+    `https://tpl-server-heroku.herokuapp.com/${category}`
+  );
+  return response.data;
 }
 
 const BookList = ({ category, userInput }) => {
+  const [state] = useAsync(() => getBooks(category), [category]);
+  const { loading, data, error } = state;
+  const [visible, setVisible] = useState(24);
 
-    const [state] = useAsync(() => getBooks(category), [category],);
-    const { loading, data, error } = state;
-    const [visible, setVisible] = useState(24);
+  const showMoreBooks = () => {
+    setVisible((prev) => prev + 24);
+  };
 
+  // Error handling.
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
+  if (!data) return null;
 
-    const showMoreBooks = () => {
-        setVisible((prev) => prev + 24)
-    }
-
-    // Error handling.
-    if (loading) return <div>Loading...</div>
-    if (error) return <div>Error</div>
-    if (!data) return null
-
-    return (
-        <section className={` ${styles['book-list']}`}>
-            <div className='wrapper'>
-                <ul className={styles['list-container']}>
-                    {/* Test filtering
+  return (
+    <section className={` ${styles['book-list']}`}>
+      <div className='wrapper'>
+        <ul className={styles['list-container']}>
+          {/* Test filtering
                     {/* Filtered by users' selected category */}
-                    {data
-                        // After being filtered by the user's selected category, It will be filtered by the user's search input.
-                        // eslint-disable-next-line array-callback-return
-                        .filter((book) => {
-                            if (book.title.replace(/\s/g, '').includes(userInput.replace(/\s/g, '').substring(0, userInput.length)) ||
-                                book.author.replace(/\s/g, '').includes(userInput.replace(/\s/g, '').substring(0, userInput.length))) {
-                                return book;
-                            }
-                        })
-                        .slice(0, visible).map((book, index) => (
-                            <Book
-                                key={book.id}
-                                cover={book.cover}
-                                title={book.title}
-                                author={book.author}
-                                address={book.link}
-                            />
-                        ))}
-                </ul>
+          {data
+            // After being filtered by the user's selected category, It will be filtered by the user's search input.
+            // eslint-disable-next-line array-callback-return
+            .filter((book) => {
+              if (
+                book.title
+                  .replace(/\s/g, '')
+                  .includes(
+                    userInput.replace(/\s/g, '').substring(0, userInput.length)
+                  ) ||
+                book.author
+                  .replace(/\s/g, '')
+                  .includes(
+                    userInput.replace(/\s/g, '').substring(0, userInput.length)
+                  )
+              ) {
+                return book;
+              }
+            })
+            .slice(0, visible)
+            .map((book, index) => (
+              <Book
+                key={book.id}
+                cover={book.cover}
+                title={book.title}
+                author={book.author}
+                address={book.link}
+              />
+            ))}
+        </ul>
 
-                <div className={styles['booklist-status']}>
-                    <h3>{visible} /{data.length}</h3>
-                    <div className={styles['progress-bar']}>
-                        <Line
-                            percent={visible / data.length * 100}
-                            trailWidth={2}
-                            strokeWidth={4}
-                            trailColor="#000000"
-                            strokeColor="#D3D3D3" />
-                    </div>
-                    <button className={styles['showMore-btn']} onClick={showMoreBooks}>Show More!</button>
-                </div>
-            </div>
-
-
-        </section>
-    )
+        <div className={styles['booklist-status']}>
+          <h3>
+            {visible} /{data.length}
+          </h3>
+          <div className={styles['progress-bar']}>
+            <Line
+              percent={(visible / data.length) * 100}
+              trailWidth={2}
+              strokeWidth={4}
+              trailColor='#000000'
+              strokeColor='#D3D3D3'
+            />
+          </div>
+          <button className={styles['showMore-btn']} onClick={showMoreBooks}>
+            <HiChevronDown
+              size='30'
+              style={{
+                position: 'absolute',
+                top: '50%',
+                left: '0',
+                transform: 'translate(10px,-50%)',
+              }}
+            />
+            <span>More results</span>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default BookList;
