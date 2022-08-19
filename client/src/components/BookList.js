e9defb5ba3270d2a7caca1db16617cd818ddaf70
woong0419 <woong0419@gmail.com>
2022-08-19 00:32:23 -0400

    edit minor details

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -6,6 +6,7 @@ import Book from './Book.js';
 import { useEffect } from 'react';
 import { HiChevronDown, HiChevronUp } from 'react-icons/hi';
 import { GiInfo } from 'react-icons/gi';
+import infoIcon from '../images/information.png';
 import { BsSearch } from 'react-icons/bs';
 
 import Loading from '../Pages/LoadingPage/Loading';
@@ -110,10 +111,10 @@ const BookList = ({ category, userInput }) => {
         {filteredData.length > 0 && (
           <>
             <div className={styles['status-container']}>
-
               <div className={styles['status-header']}>
                 <h3>
-                  토론토 공립도서관에 소장중인 4천여 권의 한국어 책을 손쉽게 찾아보세요!
+                  토론토 공립도서관에 소장중인 4천여 권의 한국어 책을 손쉽게
+                  찾아보세요!
                   <div
                     ref={wrapperRef}
                     className={styles['status-header-modal']}
@@ -122,20 +123,19 @@ const BookList = ({ category, userInput }) => {
                       onClick={infoBtnHandler}
                       className={styles['info-btn']}
                     >
-                      <GiInfo />
+                      <img src={infoIcon} alt='' />
                     </button>
                     <div
-                      className={`${showModal ? styles['modal-visible'] : ''} ${styles['info-modal']
-                        }`}
+                      className={`${showModal ? styles['modal-visible'] : ''} ${
+                        styles['info-modal']
+                      }`}
                     >
                       {showModal && <Modal setShowModal={setShowModal} />}
                     </div>
                   </div>
                 </h3>
               </div>
-              <p>
-                {filteredData.length} results in {category}
-              </p>
+              <p>{filteredData.length} results</p>
             </div>
             <ul className={styles['list-container']}>
               {filteredData.slice(0, visible).map((book, index) => (
@@ -171,8 +171,9 @@ const BookList = ({ category, userInput }) => {
 
         <button
           onClick={scrollToTop}
-          className={`${showToTop ? styles['toTop-btn-visible'] : ''} ${positionAbsolute ? styles['toTop-btn-positionAbsolute'] : ''
-            } ${styles['toTop-btn']}`}
+          className={`${showToTop ? styles['toTop-btn-visible'] : ''} ${
+            positionAbsolute ? styles['toTop-btn-positionAbsolute'] : ''
+          } ${styles['toTop-btn']}`}
         >
           <HiChevronUp className={styles['topTop-btn-svg']} size='30' />
         </button>
